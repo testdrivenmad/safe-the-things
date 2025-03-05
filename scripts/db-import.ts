83bf804633985d58ea2de98bebf452c5c0a9a154
testdrivenmad <testdrivenmad@gmail.com>
2025-03-05 17:18:10 -0600

    upgraded imports, reworked search algo to work off keywords, might have a linear time complexity

--- scripts/db-import.ts
+++ scripts/db-import.ts
@@ -1,19 +1,30 @@
-const file = Bun.file("./data/oneiowa.csv");
-console.log("file size", file.size);
-const rawData = await file.arrayBuffer();
-const decoder = new TextDecoder();
-const text = decoder.decode(rawData);
-const textArr = text.split("\r\n");
-const db: { name: string; contactInfo: string }[] = [];
-textArr.shift();
-for (const rawBusiness of textArr) {
+import existingDb from "../src/db/safe-stores.json";
+
+const csvToTextArr = async (path: string, newLineChar = "\r\n") => {
+  const file = Bun.file(path);
+  const data = await file.arrayBuffer();
+  const decoder = new TextDecoder();
+  const text = decoder.decode(data);
+  return text.split(newLineChar);
+};
+const rawData = await csvToTextArr("./data/oneiowa.csv");
+rawData.shift();
+const db = {};
+for (const rawBusiness of rawData) {
   const [name, contactInfo] = rawBusiness.split(",");
-  const business = {
-    name,
+  const newRecord = {
     contactInfo,
+    keywords: [name.toLowerCase()],
   };
-  db.push(business);
+  if (existingDb[name]) {
+    db[name] = {
+      ...existingDb[name],
+      ...newRecord,
+    };
+  }
+  db[name] = newRecord;
 }
+console.log("db", JSON.stringify(db));
 
 Bun.write("src/db/safe-stores.json", JSON.stringify(db));
 
--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,26 @@
 import { ListItem, Stack, TextField } from "@mui/material";
 import Paper from "@mui/material/Paper";
-import storeDb from "./db/safe-stores.json";
+import rawObj from "./db/safe-stores.json";
 import { FixedSizeList, ListChildComponentProps } from "react-window";
 import React from "react";
 
+const storeDb = new Map();
+const storeNameCache: string[] = [];
+const keywordToStoreNameCache = new Map<string, string[]>();
+Object.entries(rawObj).forEach(([name, metadata]) => {
+  storeNameCache.push(name);
+  storeDb.set(name, metadata);
+  for (const keyword of metadata.keywords) {
+    if (keywordToStoreNameCache.has(keyword)) {
+      keywordToStoreNameCache.get(keyword)!.push(name);
+    } else {
+      keywordToStoreNameCache.set(keyword, [name]);
+    }
+  }
+});
+
 const rowRenderer = (props: ListChildComponentProps) => {
-  const name = props.data[props.index].name;
+  const name = props.data[props.index];
   return (
     <ListItem key={name} style={props.style}>
       {name}
@@ -13,18 +28,27 @@ const rowRenderer = (props: ListChildComponentProps) => {
   );
 };
 function App() {
-  const [filteredStores, setFilteredStores] = React.useState(storeDb);
+  const [filteredStores, setFilteredStores] = React.useState(storeNameCache);
+
   return (
     <Paper sx={{ width: "100%", height: "600px" }} elevation={8}>
       <Stack direction={"column"}>
         <TextField
           onChange={(event) => {
-            if (event.target.value === undefined) setFilteredStores(storeDb);
+            if (event.target.value === undefined)
+              setFilteredStores(storeNameCache);
             else {
-              const newFilteredStores = storeDb.filter(({ name }) =>
-                name.includes(event.target.value),
-              );
-              setFilteredStores(newFilteredStores);
+              const searchTerm = event.target.value.trim();
+
+              const newFilteredStores = new Set<string>();
+              keywordToStoreNameCache.forEach((businesses, keyword) => {
+                if (keyword.includes(searchTerm)) {
+                  businesses.forEach((business) =>
+                    newFilteredStores.add(business),
+                  );
+                }
+              });
+              setFilteredStores([...newFilteredStores.values()]);
             }
           }}
         ></TextField>
@@ -33,7 +57,7 @@ function App() {
           itemSize={30}
           itemData={filteredStores}
           itemCount={filteredStores.length}
-          width={300}
+          width={"100%"}
           overscanCount={30}
         >
           {rowRenderer}
